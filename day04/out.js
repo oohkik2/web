9bb8e84767e03b5cb5644fc799b178960a670395
oohsik2 <124111023+oohkik2@users.noreply.github.com>
2023-02-02 17:35:42 +0900

    Day4

---
+++ day04/out.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script srt="out.js"></script>
+       <script> 함수(function)는 여러절차가 있는 처리를 이름을 부여하여 기능을 정의하는것....
+        function 빨래하다() {
+        
+        }
+        빨래하다() //함수를 사용, 기능을 사용하는 것
+        </script>
+  
